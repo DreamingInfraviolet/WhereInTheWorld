cb57fbfe3f2a7779d67c2f610b34f64d7703d2eb
Volodymyr Sereda <me@infraviolet.io>
2021-03-20 11:59:18 +0000

    Split map into separate file

--- src/App.js
+++ src/App.js
@@ -1,53 +1,6 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup, useMap, GeoJSON } from "react-leaflet";
-import countriesLayer from "./borders-low.json";
-// import l from 'leaflet-providers';
-
-const countryStyle = {
-  // "fillColor": "#fd6800",
-  "fillOpacity": 0.5,
-  "color": "#fd6800",
-  "opacity": 0.2,
-  "weight": 1.0,
-  // "stroke": false,
-};
-
-const WorldLayers = (props) => {
-  return [
-    <TileLayer
-      key="map"
-      attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-      url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-    />,
-    <GeoJSON key="borders" data={countriesLayer} style={(feature) => {
-      if (feature.properties.name === "Germany") return { ...countryStyle, "color": "#11ff11" };
-      else return countryStyle;
-  }}/>,
-  ];
-};
-
-const WorldMapWrapper = (props) => {
-  return (
-    <MapContainer
-      className="map"
-      center={[0, 0]}
-      zoom={3}
-      minZoom={3}
-      maxBoundsViscosity={1}
-      scrollWheelZoom={false}
-      maxBounds={[
-        [
-          [-90, -180],
-          [90, 180],
-        ],
-      ]}
-    >
-      <WorldLayers />
-    </MapContainer>
-  );
-};
+import React from "react";
+import { WorldMapWrapper } from "./map";
 
 function App() {
   return <WorldMapWrapper />;
